b232bf6337b4b3c9b26326be645b47d48537f688
Felix Vincent <michaelnwode023@gmail.com>
2025-06-19 17:45:12 +0100

    Update createToken.js

--- utils/createToken.js
+++ utils/createToken.js
@@ -9,8 +9,8 @@ export const createToken = async (id, res) => {
 
   res.cookie('token', token, {
     httpOnly: true,
-    secure: false,
-    sameSite: 'lax',
+    secure: isProd, // Only true in production
+    sameSite: isProd ? 'none' : 'lax', // 'none' allows cross-site cookies
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
